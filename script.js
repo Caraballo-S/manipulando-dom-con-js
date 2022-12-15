c08ece440e6a5a25675c0d0c63112bccf16eddc4
Sebastian <sebas.crbll@gmail.com>
2022-12-15 12:23:14 -0300

    Clase03 finalizada

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const btn = document.querySelector("[data-form-btn]");
-let input = document.querySelector("[data-form-input]");
 
- const createTask = (evento) => {
+const createTask = (evento) => {
     evento.preventDefault();
-    console.log(input.value);
-}
+    const input = document.querySelector("[data-form-input]");
+    const value = input.value;
+    input.value = "";
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li")
+    //A este li tenemos que agregarle una clase ya que tiene codigo css tambien
+    task.classList.add("card");
+    const content = `            
+        <div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>
+        </li>`
+    task.innerHTML = content;
+
+    //Con appendChild estamos diciendo al elemnto seleccionado agregarle un hijo
+    list.appendChild(task)
+
+    console.log(content);
+}; 
 console.log(btn);
 
 btn.addEventListener("click", createTask);
