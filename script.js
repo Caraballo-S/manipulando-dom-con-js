185091a09026d0c9b4f73d5944b885f4c54fd43c
Sebastian <sebas.crbll@gmail.com>
2022-12-20 17:48:34 -0300

    Accion del boton concluir y definimos el IIFE

--- script.js
+++ script.js
@@ -1,58 +1,75 @@
+( () => {
+    const btn = document.querySelector("[data-form-btn]");
 
-const btn = document.querySelector("[data-form-btn]");
+    const createTask = (evento) => {
+        evento.preventDefault();
+        const input = document.querySelector("[data-form-input]");
+        //guardo el texto en una variable
+        const value = input.value;
+        //Para limpiar el input 
+        input.value = "";
+        const list = document.querySelector("[data-list]")
+        //agrego un elemento a la variable
+        const task = document.createElement("li")
+        //agrego la clase
+        task.classList.add("card");
 
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    //guardo el texto en una variable
-    const value = input.value;
-    //Para limpiar el input 
-    input.value = "";
-    const list = document.querySelector("[data-list]")
-    //agrego un elemento a la variable
-    const task = document.createElement("li")
-    //agrego la clase
-    task.classList.add("card");
+        const taskContent = document.createElement("div")
+        //agrego un elemento html a un elemento padre
+        taskContent.appendChild(checkComplete())
+        
+        const titleTask = document.createElement("span")
+        titleTask.classList.add("task")
+        //inserto texto 
+        titleTask.innerText = value
+        
+        //agrego un elemento hijo
+        taskContent.appendChild(titleTask)
 
-    const taskContent = document.createElement("div")
-    //agrego un elemento html a un elemento padre
-    taskContent.appendChild(checkComplete())
-    
-    const titleTask = document.createElement("span")
-    titleTask.classList.add("task")
-    //inserto texto 
-    titleTask.innerText = value
-    
-    //agrego un elemento hijo
-    taskContent.appendChild(titleTask)
 
-    const deleteCard = () => {
+
+        //task.innerHTML = content;
+        
+        task.appendChild(taskContent)
+        task.appendChild(deleteIcon())
+
+        //agrego un elemento hijo
+        list.appendChild(task)
+
+        console.log(titleTask);
+    }; 
+
+
+    //Utilizo el evento con listenners (evento, accion)
+    btn.addEventListener("click", createTask);
+
+    //Creando el boton de concluido
+    const checkComplete = () => {
         const i = document.createElement("i");
-        i.classList.add("fas", "fa-trash-alt", "icon")
-
+        i.classList.add("far","fa-check-square", "icon")
+        i.addEventListener("click", completeTask);
         return i
     }
 
-    //task.innerHTML = content;
-    
-    task.appendChild(taskContent)
-    task.appendChild(deleteCard())
+    //Funcion de completar el task
+    const completeTask = (event) => {
+        const element = event.target
+        element.classList.toggle("fas");
+        element.classList.toggle("completeIcon");
+        element.classList.toggle("far");
+    }
 
-    //agrego un elemento hijo
-    list.appendChild(task)
+    //Creando el boton de borrar
+    const deleteIcon = () => {
+        const i = document.createElement("i");
+        i.classList.add("fas", "fa-trash-alt", "icon")
+        i.addEventListener("click", deleteTask);
+        return i
+    }
 
-    console.log(titleTask);
-}; 
+    const deleteTask = (event) => {
 
-console.log(btn);
+    }
 
-//Utilizo el evento con listenners (evento, accion)
-btn.addEventListener("click", createTask);
 
-//Creando el boton de concluido
-const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add("far","fa-check-square", "icon")
-
-    return i
-}
+}) ()
